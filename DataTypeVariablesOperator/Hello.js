bf3754b9d079744ef93dfc6649f10e8f2ce3d72c
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2021-01-16 13:54:01 +0600

    Code Updated

--- DataTypeVariablesOperator/Hello.js
+++ DataTypeVariablesOperator/Hello.js
@@ -1,17 +1,15 @@
-console.log('Name : Muhammed Nayeem');
+console.log('Name: Muhammed Nayeem');
 console.log('Occupation: Student');
-console.log('Title : JavaScript Programmer');
-console.log('Institute : Leading University,Sylhet');
-console.log('Department : Computer Science and Engineering(CSE)')
-console.log('Email : mohammednayeem808@gmail.com');
-console.log('Contact Number : ' + 1996436647);
+console.log('Title: JavaScript Programmer');
+console.log('Institute: Leading University, sylhet');
+console.log('Department: Computer Science and Engineering(CSE');
+console.log('Email: mohammednayeem808@gmail.com');
+console.log('Contact Number: ' + 1996436647);
 
-
-console.log('My Favorite Number : ' + 9);
-console.log( 9 + 9);
+console.log('My Favorite Number: ' + 9);
+console.log(9 + 9);
 console.log('9' + 9);
 console.log('X = '+ (9+9));
-console.log('Z = ' + (9 - 9));
-
+console.log('Z = ' + (9 -9));
 console.log('Sum is : ' + (9 + 9));
 console.log('Sub is : ' + (18 - 9));
