e5bab37fa31486d130ac11bc2cdbf75ee858b0c0
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2021-10-26 19:23:06 +0600

    JavaScript DOM

---
+++ JavaScript-DOM/dom1.js
@@ -0,0 +1,15 @@
+// Element select by Id:
+let title = document.getElementById("title");
+console.log(title);
+
+// Elements select by ClassName:
+let paragraph = document.getElementsByClassName("lead");
+console.log(paragraph);
+
+// Elements select by TagName:
+let lists = document.getElementsByTagName("li");
+console.log(lists);
+
+// Elements select by Name:
+let listItemOne = document.getElementsByName("list-item-one");
+console.log(listItemOne);
