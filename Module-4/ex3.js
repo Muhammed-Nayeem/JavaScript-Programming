b513a2f8caffa6bbbf2914bba799ee2b6f50481f
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-03-02 15:35:02 +0600

    Module Four Finished

---
+++ Module-4/ex3.js
@@ -0,0 +1,13 @@
+//comparision:
+//Every Small latter is greater than Every Capital latter;
+//In Small Latter "z" is the most greater than others and smallest is "a";
+//In Capital Latter "Z" is the most greater than others and smallest is "A";
+
+var s1 = "aaaaaaaZ";
+var s2 = "aaaaaaaz";
+
+console.log(s1 > s2);
+
+console.log('001' == 1);
+
+console.log('001' === 1);
