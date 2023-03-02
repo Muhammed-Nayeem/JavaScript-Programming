54f56effcecef42a187a55236ad28cbb39d4253f
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-03-02 21:47:25 +0600

    Module Six - Objects Finished

---
+++ Module-6/ex1.js
@@ -0,0 +1,22 @@
+//Object Literal:
+// var obj = {
+//   x: 10,
+//   y: 20,
+// };
+// console.log(obj);
+
+// obj.z = 30;
+// obj.m = 40;
+// obj['n'] = 50;
+// console.log(obj);
+
+// console.log(obj.x);
+
+// console.log(obj.d);
+
+//Object Constructor:
+var obj2 = Object();
+obj2.x = 10;
+obj2.y = 20;
+obj2.z = 30;
+console.log(obj2);
---
+++ Module-6/ex4.js
@@ -0,0 +1,15 @@
+//setting object properties:
+var me = {
+  name: "Muhammed",
+  age: 25,
+  profession: "Developer",
+};
+console.log(me);
+
+me.name = "Muhammed Nayeem";
+me.profession = "Software Developer";
+console.log(me);
+
+me['email'] = "nayeem@gmail.com";
+me['phone'] = "01703479326";
+console.log(me);
