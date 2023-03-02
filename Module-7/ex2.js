be87e9556b8326142e64e13bc77cf4c47b0c3a49
Muhammed Nayeem <mohammednayeem808@gmail.com>
2023-03-02 22:10:40 +0600

    Module Seven - Function Finished

---
+++ Module-7/ex2.js
@@ -0,0 +1,8 @@
+//parameters and arguments:
+function addition(num1, num2, num3) {
+  var sum = num1 + num2 + num3;
+  console.log(sum);
+}
+
+//passing arguments when called function:
+addition(10, 20, 30);
---
+++ Module-7/ex4.js
@@ -0,0 +1,8 @@
+//return something from function:
+function addition(num1, num2, num3) {
+  var sum = num1 + num2 + num3;
+  return sum;
+}
+
+var result = addition(10, 20, 30);
+console.log(result);
